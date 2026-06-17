dcdae34360d25ec81e4d6cee997911276058eaee
josephine67-6822 <josephine.wa.67@ubu.ac.th>
2026-06-17 10:05:35 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnCS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a =5; int b =2;
+            Console.WriteLine($"a + b = {a + b}");
+            // Addition
+        }
+    }
+}
